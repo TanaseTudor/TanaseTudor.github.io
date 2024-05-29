065d8cae8ae5ae0e7f80239400cc462b312dc971
TanaseTudor <163713702+TanaseTudor@users.noreply.github.com>
2024-05-29 20:53:34 +0300

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,20 @@
+function LoginPage() {
+    let name = document.getElementById("q").value;
+    let password = document.getElementById("w").value;
+
+    if(name == "tudor") {
+        if(password == "3894") {
+            
+                location.href = "conectat.html"
+            
+        }
+    }
+
+    if(name == "rares") {
+        if(password == "2431") {
+            
+                location.href = "conectat.html"
+            
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+function MediiScolare() {
+    location.href = "MediiScolare.html"
+}
+
+function Proiecte() {
+    location.href = "Proiecte.html"
+}
+
+function Note() {
+    location.href = "Note.html"
+}
+
+function Joc() {
+    location.href = "Joc.html"
+}
+
+function Joc2() {
+    location.href = "GhicesteNumarul.html"
+}
